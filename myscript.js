1b7dec51c8c057939555dd1651d8b39a723259f6
Techqurious <68885055+manish202@users.noreply.github.com>
2020-12-16 18:00:57 +0530

    this is my first repository

---
+++ myscript.js
@@ -0,0 +1,40 @@
+$(function(){
+    $(window).scroll(function(){
+        var top = $(window).scrollTop();
+        if(top >= 500){
+            $('.go-up').slideDown(500);
+        }else{
+            $('.go-up').slideUp(500);
+        }
+    });
+
+    $('.go-up').click(function(){
+        $(window).scrollTop(0);
+    });
+
+    $('#email').keyup(function(){
+        var email = $('#email').val();
+        if(isValidEmail(email)){
+            $('#email').removeClass('is-invalid').addClass('is-valid');
+        }else{
+            $('#email').removeClass('is-valid').addClass('is-invalid');
+        }
+    });
+    $('#subject').keyup(function(){
+        var subject = $('#subject').val();
+        if(subject.length <= 100){
+            $('#subject').removeClass('is-invalid').addClass('is-valid');
+        }else{
+            $('#subject').removeClass('is-valid').addClass('is-invalid');
+        }
+    });
+    
+});
+
+function isValidEmail(param){
+    if(param.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)){
+        return true;
+    }else{
+        return false;
+    }
+}
